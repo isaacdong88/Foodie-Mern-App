515be998640e7adf5d6ce9a2c8d843b94c3c53c1
isaacdong88 <isaacdong88@gmail.com>
2023-04-12 14:08:16 -0400

    set up routes and controller for reviews

--- backend/controllers/ReviewsController.js
+++ backend/controllers/ReviewsController.js
@@ -9,8 +9,37 @@ const fetchReviews = async (req, res) => {
   }
 };
 
+//Show reviews, route Post /reviews
+const postReviews = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Show reviews" });
+  } catch (error) {
+    res.status(400).json({ message: "Can't find reviews" });
+  }
+};
+
+//Edit reviews, route Put /reviews/:id
+const editReviews = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Get reviews" });
+  } catch (error) {
+    res.status(400).json({ message: "Can't find reviews" });
+  }
+};
+
+//Delete reviews, route Delete /reviews/:id
+const deleteReviews = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Get reviews" });
+  } catch (error) {
+    res.status(400).json({ message: "Can't find reviews" });
+  }
+};
 module.exports = {
   fetchReviews,
+  postReviews,
+  editReviews,
+  deleteReviews,
 };
 
 // module.exports = {
--- backend/routes/Reviews.js
+++ backend/routes/Reviews.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { fetchReviews } = require("../controllers/ReviewsController");
+const {
+  fetchReviews,
+  postReviews,
+  editReviews,
+  deleteReviews,
+} = require("../controllers/ReviewsController");
 
 // router.get("/", controller.get);
 router.get("/", fetchReviews);
-router.post("/", controller.post);
-router.put("/:id", controller.put);
-router.delete("/:id", controller.delete);
+router.post("/", postReviews);
+router.put("/:id", editReviews);
+router.delete("/:id", deleteReviews);
 
 module.exports = router;
