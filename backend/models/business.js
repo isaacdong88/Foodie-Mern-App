872ecfbc2773803fc792843806e504d57f4f8ae8
isaacdong88 <isaacdong88@gmail.com>
2023-04-13 09:40:00 -0400

    install packages for password encryption and user auth

--- backend/models/business.js
+++ backend/models/business.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const businessSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
-  reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Reviews" },
+  email: { type: String, required: true, unique: true },
+  // reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Reviews" },
 });
 
 const Business = mongoose.model("Business", businessSchema);
--- backend/models/customer.js
+++ backend/models/customer.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const customerSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
-  reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Reviews" },
+  email: { type: String, required: true, unique: true },
+  // reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Reviews" },
 });
 
 const Customers = mongoose.model("Customer", customerSchema);
