ab83396ecb0efb5b854f802f94185ab57e576cf5
isaacdong88 <isaacdong88@gmail.com>
2023-04-12 23:46:23 -0400

    CRUD for business route completed

--- backend/controllers/BusinessController.js
+++ backend/controllers/BusinessController.js
@@ -2,7 +2,8 @@ const Business = require("../models/business");
 //Get business, route GET /business
 const fetchBusiness = async (req, res) => {
   try {
-    res.status(200).json({ method: "Get Business" });
+    const business = await Business.find();
+    res.status(200).json(business);
   } catch (error) {
     res.status(400).json({ method: "Invalid Business" });
   }
@@ -11,7 +12,12 @@ const fetchBusiness = async (req, res) => {
 //Create business, route Post /business
 const createBusiness = async (req, res) => {
   try {
-    res.status(200).json({ method: "Create Business" });
+    const createBusiness = await Business.create({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    });
+    res.status(200).json(createBusiness);
   } catch (error) {
     res.status(400).json({ method: "Invalid Business" });
   }
@@ -20,7 +26,11 @@ const createBusiness = async (req, res) => {
 //Edit business, route Put /business
 const editBusiness = async (req, res) => {
   try {
-    res.status(200).json({ method: "Edit Business" });
+    const editBusiness = await Business.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json(editBusiness);
   } catch (error) {
     res.status(400).json({ method: "Invalid Business" });
   }
@@ -29,7 +39,8 @@ const editBusiness = async (req, res) => {
 //Delete business, route Delete /business
 const deleteBusiness = async (req, res) => {
   try {
-    res.status(200).json({ method: "Delete Business" });
+    const deleteBusiness = await Business.findByIdAndRemove(req.params.id);
+    res.status(200).json({ id: req.params.id });
   } catch (error) {
     res.status(400).json({ method: "Invalid Business" });
   }
