5084bd3448c9f5a5c800260632a10eaa3b620824
isaacdong88 <isaacdong88@gmail.com>
2023-04-14 12:07:09 -0400

    setup pages, components, and react routes

--- backend/controllers/ReviewsController.js
+++ backend/controllers/ReviewsController.js
@@ -16,7 +16,7 @@ const createReviews = async (req, res) => {
     const reviews = await Review.create({
       review: req.body.review,
       rating: req.body.rating,
-      // user: req.user.id,
+      user: req.user.id,
     });
     res.status(200).json(reviews);
   } catch (error) {
---
+++ frontend/src/component/Interface.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Interface() {
+  return (
+    <div>Interface</div>
+  )
+}
+
+export default Interface
