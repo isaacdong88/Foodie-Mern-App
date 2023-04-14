12d880f25872d6d49e53b0e3412fb7831b581b0c
isaacdong88 <isaacdong88@gmail.com>
2023-04-13 22:06:07 -0400

    added token for use authorization

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -9,7 +9,12 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user && (await bcrypt.compare(password, user.password))) {
-      res.status(200).json(user);
+      res.status(200).json({
+        _id: user.id,
+        username: user.username,
+        email: user.email,
+        token: genToken(user._id),
+      });
     } else {
       res.status(400).json({ message: "Invalid Credentials" });
     }
