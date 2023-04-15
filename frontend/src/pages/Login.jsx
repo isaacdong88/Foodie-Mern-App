46d96005f55cc00ded951e08731d5d632704e584
isaacdong88 <isaacdong88@gmail.com>
2023-04-14 23:11:37 -0400

    complete login form and handle submit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
 
 function Login() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const {email, password} = formData
+
+    // handleChange - updates formData when we type into form
+    const handleChange = (event) => {
+    //use the event object to detect key and value to update
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+        console.log(formData)
+    };
+
+    const handleSubmit = (event) => {
+        //prevent page from refreshing on form submission
+        event.preventDefault();
+        console.log(formData,'*')
+    };
   return (
-    <div>Login</div>
+    <div>
+        <form onSubmit={handleSubmit} action="">
+            <h1>Customer Login</h1>
+            <div>
+                <input type="text" id="email" name="email" value={email} placeholder='Email' onChange={handleChange}/>
+            </div>
+            <div>
+                <input type="password" id="password" name="password" value={password} placeholder='Password' onChange={handleChange}/>
+            </div>
+            <div>
+                <button type='submit'>Submit</button>
+            </div>
+        </form>
+    </div>
+
   )
 }
 
